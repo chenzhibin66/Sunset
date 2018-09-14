d9706378f15308c9dc5852593752fabb925fc61d
chen <m15903462872@163.com>
2018-09-14 20:29:31 +0800

    Initial commit

---
+++ app/src/main/java/example/binean/com/sunset/SunsetActivity.java
@@ -0,0 +1,13 @@
+
+package example.binean.com.sunset;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SunsetActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
